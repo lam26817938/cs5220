c95197ce8d7f27f7c14dbc5c468828bd029b228a
Lian Liao <ll987@login28.chn.perlmutter.nersc.gov>
2024-01-25 21:48:12 -0800

    ft

---
+++ hw1/build/CMakeFiles/benchmark-naive.dir/dgemm-naive.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-naive.dir/dgemm-naive.c.o: \
+ /global/homes/l/ll987/cs5220/hw1/dgemm-naive.c \
+ /usr/include/stdc-predef.h
---
+++ hw1/build/CPackConfig.cmake
@@ -0,0 +1,66 @@
+# This file will be configured to contain variables for CPack. These variables
+# should be set in the CMake list file of the project before CPack module is
+# included. The list of available CPACK_xxx variables and their associated
+# documentation may be obtained using
+#  cpack --help-variable-list
+#
+# Some variables are common to all generators (e.g. CPACK_PACKAGE_NAME)
+# and some are specific to a generator
+# (e.g. CPACK_NSIS_EXTRA_INSTALL_COMMANDS). The generator specific variables
+# usually begin with CPACK_<GENNAME>_xxxx.
+
+
+set(CPACK_BUILD_SOURCE_DIRS "/global/homes/l/ll987/cs5220/hw1;/global/homes/l/ll987/cs5220/hw1/build")
+set(CPACK_CMAKE_GENERATOR "Unix Makefiles")
+set(CPACK_COMPONENT_UNSPECIFIED_HIDDEN "TRUE")
+set(CPACK_COMPONENT_UNSPECIFIED_REQUIRED "TRUE")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_FILE "/global/common/software/nersc/pm-2021q4/sw/cmake-3.22.0/share/cmake-3.22/Templates/CPack.GenericDescription.txt")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_SUMMARY "hw1 built using CMake")
+set(CPACK_GENERATOR "TGZ")
+set(CPACK_INSTALL_CMAKE_PROJECTS "/global/homes/l/ll987/cs5220/hw1/build;hw1;ALL;/")
+set(CPACK_INSTALL_PREFIX "/usr/local")
+set(CPACK_MODULE_PATH "")
+set(CPACK_NSIS_DISPLAY_NAME "hw1 0.1.1")
+set(CPACK_NSIS_INSTALLER_ICON_CODE "")
+set(CPACK_NSIS_INSTALLER_MUI_ICON_CODE "")
+set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES")
+set(CPACK_NSIS_PACKAGE_NAME "hw1 0.1.1")
+set(CPACK_NSIS_UNINSTALL_NAME "Uninstall")
+set(CPACK_OUTPUT_CONFIG_FILE "/global/homes/l/ll987/cs5220/hw1/build/CPackConfig.cmake")
+set(CPACK_PACKAGE_DEFAULT_LOCATION "/")
+set(CPACK_PACKAGE_DESCRIPTION_FILE "/global/common/software/nersc/pm-2021q4/sw/cmake-3.22.0/share/cmake-3.22/Templates/CPack.GenericDescription.txt")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "hw1 built using CMake")
+set(CPACK_PACKAGE_FILE_NAME "CS5220Group069_hw1")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "hw1 0.1.1")
+set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "hw1 0.1.1")
+set(CPACK_PACKAGE_NAME "hw1")
+set(CPACK_PACKAGE_RELOCATABLE "true")
+set(CPACK_PACKAGE_VENDOR "Humanity")
+set(CPACK_PACKAGE_VERSION "0.1.1")
+set(CPACK_PACKAGE_VERSION_MAJOR "0")
+set(CPACK_PACKAGE_VERSION_MINOR "1")
+set(CPACK_PACKAGE_VERSION_PATCH "1")
+set(CPACK_RESOURCE_FILE_LICENSE "/global/common/software/nersc/pm-2021q4/sw/cmake-3.22.0/share/cmake-3.22/Templates/CPack.GenericLicense.txt")
+set(CPACK_RESOURCE_FILE_README "/global/common/software/nersc/pm-2021q4/sw/cmake-3.22.0/share/cmake-3.22/Templates/CPack.GenericDescription.txt")
+set(CPACK_RESOURCE_FILE_WELCOME "/global/common/software/nersc/pm-2021q4/sw/cmake-3.22.0/share/cmake-3.22/Templates/CPack.GenericWelcome.txt")
+set(CPACK_SET_DESTDIR "OFF")
+set(CPACK_SOURCE_GENERATOR "TBZ2;TGZ;TXZ;TZ")
+set(CPACK_SOURCE_OUTPUT_CONFIG_FILE "/global/homes/l/ll987/cs5220/hw1/build/CPackSourceConfig.cmake")
+set(CPACK_SOURCE_RPM "OFF")
+set(CPACK_SOURCE_TBZ2 "ON")
+set(CPACK_SOURCE_TGZ "ON")
+set(CPACK_SOURCE_TXZ "ON")
+set(CPACK_SOURCE_TZ "ON")
+set(CPACK_SOURCE_ZIP "OFF")
+set(CPACK_SYSTEM_NAME "Linux")
+set(CPACK_THREADS "1")
+set(CPACK_TOPLEVEL_TAG "Linux")
+set(CPACK_WIX_SIZEOF_VOID_P "8")
+
+if(NOT CPACK_PROPERTIES_FILE)
+  set(CPACK_PROPERTIES_FILE "/global/homes/l/ll987/cs5220/hw1/build/CPackProperties.cmake")
+endif()
+
+if(EXISTS ${CPACK_PROPERTIES_FILE})
+  include(${CPACK_PROPERTIES_FILE})
+endif()
