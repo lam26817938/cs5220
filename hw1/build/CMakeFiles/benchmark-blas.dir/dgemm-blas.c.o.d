b5a7d84e706e3ae971f3e0b8f23b9ec9f101b362
Lian Liao <ll987@login28.chn.perlmutter.nersc.gov>
2024-01-25 21:48:12 -0800

    ft

---
+++ hw1/build/CMakeFiles/benchmark-blas.dir/dgemm-blas.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/benchmark-blas.dir/dgemm-blas.c.o: \
+ /global/homes/l/ll987/cs5220/hw1/dgemm-blas.c /usr/include/stdc-predef.h \
+ /opt/cray/pe/libsci/23.12.5/GNU/12.3/x86_64/include/cblas.h \
+ /usr/lib64/gcc/x86_64-suse-linux/12/include/stddef.h
---
+++ hw1/build/CMakeFiles/benchmark-blocked.dir/dgemm-blocked.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-blocked.dir/dgemm-blocked.c.o: \
+ /global/homes/l/ll987/cs5220/hw1/dgemm-blocked.c \
+ /usr/include/stdc-predef.h
