6b451dcb1970c101e03134cf47342237713d2de3
Lian Liao <ll987@login31.chn.perlmutter.nersc.gov>
2024-02-12 21:57:09 -0800

    extra

---
+++ hw2/build/CMakeFiles/openmp.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/global/u1/l/ll987/cs5220/hw2/main.cpp" "CMakeFiles/openmp.dir/main.cpp.o" "gcc" "CMakeFiles/openmp.dir/main.cpp.o.d"
+  "/global/u1/l/ll987/cs5220/hw2/openmp.cpp" "CMakeFiles/openmp.dir/openmp.cpp.o" "gcc" "CMakeFiles/openmp.dir/openmp.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hw2/build/CMakeFiles/openmp.dir/openmp.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/openmp.dir/openmp.cpp.o: \
+ /global/u1/l/ll987/cs5220/hw2/openmp.cpp /usr/include/stdc-predef.h \
+ /global/u1/l/ll987/cs5220/hw2/common.h \
+ /usr/lib64/gcc/x86_64-suse-linux/12/include/omp.h
